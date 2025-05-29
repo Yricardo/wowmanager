fb2ccb8942ee3a9a821e442e97ff2730337c3018
Yricardo <yoannricard@gmail.com>
2025-05-30 00:12:35 +0200

    Update InvitationTypeForm.php

--- src/Form/InvitationTypeForm.php
+++ src/Form/InvitationTypeForm.php
@@ -9,7 +9,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class InvitationType extends AbstractType
+class InvitationTypeForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
